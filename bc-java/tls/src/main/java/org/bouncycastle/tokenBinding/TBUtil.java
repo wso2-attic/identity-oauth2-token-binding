da273577bf920ea19e68520fc46f05738102c7bf
Inthirakumaaran <inthiraj1994@gmail.com>
2017-12-08 18:39:30 +0530

    created TokenBinding Exception

--- bc-java/tls/src/main/java/org/bouncycastle/tokenBinding/TBUtil.java
+++ bc-java/tls/src/main/java/org/bouncycastle/tokenBinding/TBUtil.java
@@ -17,52 +17,78 @@ public class TBUtil {
     }
 
     public static void writeOpaque8(byte[] buf, OutputStream output)
-            throws IOException
+            throws TokenBindingException
     {
         checkUint8(buf.length);
         writeUint8(buf.length, output);
-        output.write(buf);
-    }
-
-    public static void writeOpaque16(byte[] buf, OutputStream output)
-            throws IOException
-    {
-        checkUint16(buf.length);
-        writeUint16(buf.length, output);
-        output.write(buf);
-    }
-
-    public static void checkUint16(int i) throws IOException
-    {
-        if (!isValidUint16(i))
-        {
-            throw new IOException("checkUnit16 failed");
+        try {
+            output.write(buf);
+        }catch (IOException e){
+            throw new TokenBindingException(e.getMessage());
         }
     }
 
-    public static void checkUint8(int i) throws IOException
+    public static void writeOpaque16(byte[] buf, OutputStream output)
+            throws TokenBindingException
+    {
+        checkUint16(buf.length);
+        writeUint16(buf.length, output);
+        try {
+            output.write(buf);
+        }catch (IOException e){
+            throw new TokenBindingException(e.getMessage());
+        }
+    }
+
+    public static void checkUint16(int i) throws TokenBindingException
+    {
+        if (!isValidUint16(i))
+        {
+            throw new TokenBindingException("checkUnit16 failed");
+//            throw new TokenBindingException("checkUnit16 failed");
+            
+        }
+    }
+
+    public static void checkUint8(int i) throws TokenBindingException
     {
         if (!isValidUint8(i))
         {
-            throw new IOException("checkUnit8 failed");
+            throw new TokenBindingException("checkUnit8 failed");
         }
     }
 
     public static void writeUint8(int i, OutputStream output)
-            throws IOException
+            throws TokenBindingException
     {
-        output.write(i);
+        try {
+            output.write(i);
+        }catch (IOException e){
+            throw new TokenBindingException(e.getMessage());
+        }
     }
 
     public static void writeUint16(int i, OutputStream output)
-            throws IOException
+            throws TokenBindingException
     {
-        output.write(i >>> 8);
-        output.write(i);
+        try {
+            output.write(i >>> 8);
+            output.write(i);
+        }catch (IOException e){
+            throw new TokenBindingException(e.getMessage());
+        }
     }
     public static void writeBufTo(ByteArrayOutputStream buf, OutputStream output)
-            throws IOException
+            throws TokenBindingException
     {
-        buf.writeTo(output);
+        try {
+            buf.writeTo(output);
+        }catch (IOException e){
+            throw new TokenBindingException(e.getMessage());
+        }
     }
+
+
+
 }
+
---
+++ bc-java/tls/src/main/java/org/bouncycastle/tokenBinding/TokenBindingException.java
@@ -0,0 +1,11 @@
+package org.bouncycastle.tokenBinding;
+
+import java.io.IOException;
+
+class TokenBindingException extends IOException {
+
+
+    public TokenBindingException(String message) {
+        super(message);
+    }
+}
